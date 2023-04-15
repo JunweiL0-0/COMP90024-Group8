573a65114e4787a5a8df0ab64e7cb382b996dba5
JunweiL0-0 <jli328@uclive.ac.nz>
2023-04-15 15:39:49 +1000

    modify controller

--- backend/app/index_controller.py
+++ backend/app/index_controller.py
@@ -1,3 +1,9 @@
 class IndexController:
+    request = None
+
+    def __init__(self, request):
+        self.request = request
+
     def get_hello_world(self):
-        return "{message: \"Hello world\"}"
+        # return {'ip': str(self.request.args.get("haha"))}, 200
+        return {'message': 'hello world'}, 200
